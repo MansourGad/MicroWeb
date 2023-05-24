0f7ee42bb6253fb3fb68f8f6a3d009ad7f6aabe3
Mansour <93281482+MansourGad@users.noreply.github.com>
2023-05-24 17:16:17 +0300

    Add files via upload

---
+++ Home.js
@@ -0,0 +1,22 @@
+// // window.alert("Mansour");
+
+// console.log("Mansour gad Company for Progs");
+
+// console.error("Error");
+
+// console.table(["Mansour", "Yahia", "Hassan"]);
+
+// var myname = "Mansour";
+// console.log("Hello" +  myname);
+
+
+
+// $(window).scroll(function(){
+//     if($(window).scrollTop()){
+//         $("nav").addClass("black")
+//     }
+
+//     else{
+//         $("nav").removeClass("black")
+//     }
+// });
